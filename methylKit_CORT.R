83c6721f048f5743a28ea5ab3742555bba16c636
Jaeyoon Cha <jaeyoonc@princeton.edu>
2020-11-06 15:02:28 -0600

    load CORT and VEHICLE (rr01-rr10) as methylKit obj

---
+++ methylKit_CORT.R
@@ -0,0 +1,79 @@
+library(methylKit)
+######## read in rr05 .cov file ########
+
+# cort.list has rr01-rr05 CORT and rr06-rr10 VEHICLE
+cort.list=list("~/Desktop/THESIS/covs/G695_rr01_S15_val_1_bismark_bt2_pe.bismark.cov",
+               "~/Desktop/THESIS/covs/G695_rr02_S16_val_1_bismark_bt2_pe.bismark.cov",
+               "~/Desktop/THESIS/covs/G695_rr03_S17_val_1_bismark_bt2_pe.bismark.cov",
+               "~/Desktop/THESIS/covs/G695_rr04_S18_val_1_bismark_bt2_pe.bismark.cov",
+               "~/Desktop/THESIS/covs/G695_rr05_S19_val_1_bismark_bt2_pe.bismark.cov",
+               "~/Desktop/THESIS/covs/G695_rr06_S20_val_1_bismark_bt2_pe.bismark.cov",
+               "~/Desktop/THESIS/covs/G695_rr07_S21_val_1_bismark_bt2_pe.bismark.cov",
+               "~/Desktop/THESIS/covs/G695_rr08_S22_val_1_bismark_bt2_pe.bismark.cov",
+               "~/Desktop/THESIS/covs/G695_rr09_S23_val_1_bismark_bt2_pe.bismark.cov",
+               "~/Desktop/THESIS/covs/G695_rr10_S24_val_1_bismark_bt2_pe.bismark.cov")
+
+# cort is CORT and VEHICLE
+cort=methRead(cort.list,
+              sample.id = list("rr01", "rr02", "rr03", "rr04", "rr05", "rr06", "rr07", "rr08", "rr09", "rr10"),
+              treatment = c(1,1,1,1,1,0,0,0,0,0),
+              pipeline = "bismarkCoverage",
+              header = FALSE,
+              assembly="GRCm38")
+
+getMethylationStats(cort[[2]],plot=FALSE,both.strands=FALSE)
+getMethylationStats(cort[[10]],plot=TRUE,both.strands=FALSE)
+getCoverageStats(cort[[1]],plot=TRUE,both.strands=FALSE)
+
+
+
+
+
+
+
+# (2.5) filter away low coverage
+# discards bases that have coverage below 10X 
+# also discards bases with > 99.9th percentile of coverage in each sample
+filtered.myobj=filterByCoverage(myobj,lo.count=10,lo.perc=NULL,
+                                hi.count=NULL,hi.perc=99.9)
+
+getMethylationStats(filtered.myobj[[1]],plot=TRUE,both.strands=FALSE)
+
+getCoverageStats(filtered.myobj[[1]],plot=TRUE,both.strands=FALSE)
+
+
+
+
+
+########### 2020.11.02 attempting to sort cov file by chr ###########
+# # sort rr05 then read into file.list
+# rr05_sort <- rr05[with(rr05, order(V1)), ]
+# 
+# # rename rows
+# rownames(rr05_sort) <- c(1:nrow(rr05))
+# 
+# # save
+# write.table(rr05_sort, file = "~/Desktop/THESIS/nomismatch.rr05.sorted.cov", 
+#             quote = FALSE, row.names = FALSE, sep = "\t")
+# 
+# # here, went into Terminal and deleted the header column names in sorted.cov
+# 
+# # now make new file.list.sorted
+# file.list_sort=list("~/Desktop/THESIS/nomismatch.rr05.sorted.cov")
+# class(file.list_sort)
+# 
+# # this works!
+# myobj_sort=methRead(file.list_sort,
+#                     sample.id=list("rr05"),
+#                     pipeline = "bismarkCoverage",
+#                     header = FALSE,
+#                     assembly="GRCm38",treatment=1)
+
+# but -- think that coverage file does NOT need to be sorted for methylKit, so no need.
+
+################# inputing BAM files #################
+
+# my.meth.bam<-processBismarkAln(
+#   "~/Desktop/THESIS/G695_rr01_S15_val_1_bismark_bt2_pe.sorted.bam",
+#   sample.id = "rr01",
+#   assembly = "mm10")
